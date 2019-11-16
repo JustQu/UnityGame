86254773396491aaf28e1bf33a4029fcb309f630
JustQu <cool.3meu@gmail.com>
2019-11-16 15:34:25 +0300

    dungeon generation

---
+++ Scripts/EnterDoor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnterDoor : MonoBehaviour
+{
+	private void OnCollisionEnter2D(Collision2D other) {
+		if (other.gameObject.CompareTag("Player")){
+			GameObject dungeon = GameObject.FindGameObjectWithTag("Dungeon");
+			DungeonGenerator dungeonGenerator = dungeon.GetComponent<DungeonGenerator>();
+			Room room = dungeonGenerator.CurrentRoom();
+			dungeonGenerator.MoveToRoom(room.Neighbor((this.name[5]).ToString()));
+			SceneManager.LoadScene("Demo");
+		}
+	}
+}
