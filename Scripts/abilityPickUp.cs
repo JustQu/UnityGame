42770936a343abe65b4da27c3c94cd7381325317
Borislav <borislavfra@gmail.com>
2019-11-17 08:11:34 +0300

    abilities pickable

---
+++ Scripts/abilityPickUp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class abilityPickUp : MonoBehaviour
+{
+    public Ability ability;
+    public AbilityCoolDown aSlot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            aSlot.ability = ability;
+        }
+    }
+}
